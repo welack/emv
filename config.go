201846d7316dd53f068432db1ea7e94301a96354
radhitya <alif@radhitya.org>
2026-09-04 11:28:42 +0700

    init

---
+++ config.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+type Account struct {
+	IMAPHost string `json:"imap_host"`
+	IMAPPort string `json:"imap_port"`
+	SMTPHost string `json:"smtp_host"`
+	SMTPPort string `json:"smtp_port"`
+	User     string `json:"user"`
+}
+
+func load_config(app_name string) (*Account, error) {
+	config_dir, err := os.UserConfigDir()
+	if err != nil {
+		return nil, fmt.Errorf("could not find user config dir: %w", err)
+	}
+
+	config_path := filepath.Join(config_dir, app_name, "config.json")
+
+	file_bytes, err := os.ReadFile(config_path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read config file: %w", err)
+	}
+
+	var acc Account
+	if err := json.Unmarshal(file_bytes, &acc); err != nil {
+		return nil, fmt.Errorf("failed to parse config JSON: %w", err)
+	}
+
+	return &acc, nil
+}
+
+func save_config(app_name string, acc *Account) error {
+	config_dir, err := os.UserConfigDir()
+	if err != nil {
+		return fmt.Errorf("could not find user config dir: %w", err)
+	}
+
+	path := filepath.Join(config_dir, app_name, "config.json")
+	if err := os.MkdirAll(filepath.Dir(path), 0o700); err != nil {
+		return fmt.Errorf("could not create config dir: %w", err)
+	}
+	b, err := json.MarshalIndent(acc, "", "  ")
+	if err != nil {
+		return fmt.Errorf("failed to encode config: %w", err)
+	}
+	return os.WriteFile(path, b, 0o600)
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module emv
+
+go 1.24.4
+
+require (
+	fyne.io/fyne/v2 v2.8.1 // indirect
+	github.com/emersion/go-imap/v2 v2.0.0-beta.8 // indirect
+	github.com/k3a/html2text v1.4.0 // indirect
+)
